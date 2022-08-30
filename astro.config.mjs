ef82b206546a6723b516b635367eaf87170b179a
Arman Hashemi <ahhdev@gmail.com>
2022-08-30 11:47:06 +0430

    remove astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import node from "@astrojs/node";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node(),
-  integrations: [react(), image(), compress()],
+  integrations: [react(), image()],
 });
